d749e3c9caa92935c2be08eab2ccf4386412454a
camm22 <cambordes001@gmail.com>
2023-12-07 23:34:53 +0100

    modify db.js

--- backend/app/config/db.js
+++ backend/app/config/db.js
@@ -7,10 +7,10 @@ module.exports = {
     dialect: "mysql",
 
     pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
     }
-    };
+};
 
